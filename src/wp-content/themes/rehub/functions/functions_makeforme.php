ad2a169ae50ac54fa81ef51c2653016576738d0b
Elvis Martins <elvisbmartins@gmail.com>
2016-12-31 18:48:13 -0200

    add functions_makeforme theme redhub

--- src/wp-content/themes/rehub/functions/functions_makeforme.php
+++ src/wp-content/themes/rehub/functions/functions_makeforme.php
@@ -1,1 +1,18 @@
 <?php 
+
+
+
+//Header Top Widget
+function makeforme_register_sidebars() {
+
+	register_sidebar(array(
+		'id' => 'header-top',
+		'name' => __('Sidebar Top Header', ''),
+		'before_widget' => '<div id="%1$s" class="widget %2$s">',
+		'after_widget' => '</div>',
+		'before_title' => '<div class="title">',
+		'after_title' => '</div>',
+	));
+}
+
+add_action( 'widgets_init', 'makeforme_register_sidebars' );
