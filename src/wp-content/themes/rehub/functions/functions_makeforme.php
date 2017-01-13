ee79893c717002fa37b5d69cd43312e5538c769e
Elvis Martins <elvisbmartins@gmail.com>
2017-01-13 11:47:19 -0200

    function makeforme pagarme receiver

--- src/wp-content/themes/rehub/functions/functions_makeforme.php
+++ src/wp-content/themes/rehub/functions/functions_makeforme.php
@@ -25,3 +25,36 @@ function makeforme_register_sidebars() {
 }
 
 add_action( 'widgets_init', 'makeforme_register_sidebars' );
+
+
+
+//
+function request_receiver_pagarme( $receiver_id ){
+
+	$api_url = "https://api.pagar.me/1/recipients/".$receiver_id."/balance";
+
+	$api_key = "ak_test_Oq6uuxxWJB4WFCbWC5cFOaM6sjYvvx";
+
+	$data = array( 'api_key' => $api_key );
+
+	$params = array(
+		'method'  => 'GET',
+		'timeout' => 60,
+	);
+
+	if ( ! empty( $data ) ) {
+		$params['body'] = $data;
+	}
+
+	if ( ! empty( $headers ) ) {
+		$params['headers'] = $headers;
+	}
+
+	$response = wp_safe_remote_post( $api_url, $params );
+	$response = json_decode( $response['body'] );
+
+	return $response;
+}
+
+
+
