f065e248161092ae906333372451f9ecf7ac2128
Elvis Martins <elvisbmartins@gmail.com>
2017-02-01 16:45:14 -0200

    layout menu configurações e configurações loja

--- src/wp-content/themes/rehub/template-fullwidth.php
+++ src/wp-content/themes/rehub/template-fullwidth.php
@@ -15,7 +15,7 @@
 		<div class="clearfix">
 		      <!-- Main Side -->
               <div class="main-side page clearfix full_width">
-                <div class="title"><h1><?php the_title(); ?></h1></div>
+           
                 <article class="post" id="page-<?php the_ID(); ?>">				
 				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
                   <?php the_content(); ?>
