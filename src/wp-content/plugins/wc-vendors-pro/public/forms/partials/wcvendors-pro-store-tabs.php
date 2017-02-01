d1a183f6f0d13b515a765ab2f24ff80da9f1eafa
Elvis Martins <elvisbmartins@gmail.com>
2017-02-01 18:54:06 -0200

    IMPORTANTE alteração wcvendors-pro-store-tabs para definir organização das tabs de configuração

--- src/wp-content/plugins/wc-vendors-pro/public/forms/partials/wcvendors-pro-store-tabs.php
+++ src/wp-content/plugins/wc-vendors-pro/public/forms/partials/wcvendors-pro-store-tabs.php
@@ -14,7 +14,17 @@
 ?>
 
 <ul class="<?php echo $css_class; ?>" style="padding:0; margin:0;">
-	<?php foreach ( $store_tabs as $tab ) : ?>
-	    <li><a class="tabs-tab <?php echo implode( ' ' , $tab[ 'class' ] ); ?> <?php echo $tab[ 'target' ]; ?>" href="#<?php echo $tab[ 'target' ]; ?>"><?php echo $tab[ 'label' ]; ?></a></li>
-	<?php endforeach; ?>        
+	<?php 
+		/* foreach ( $store_tabs as $tab ) : 
+	   	 <li><a class="tabs-tab <?php echo implode( ' ' , $tab[ 'class' ] ); ?> <?php echo $tab[ 'target' ]; ?>" href="#<?php echo $tab[ 'target' ]; ?>"><?php echo $tab[ 'label' ]; ?></a></li>
+		endforeach; 
+		*/ 
+	?>  
+	<li><a class="tabs-tab store" href="#store">Dados da loja</a></li>
+	<li><a class="tabs-tab branding" href="#branding">Banner</a></li>
+	<li><a class="tabs-tab adress" href="#adress">Endereço</a></li>
+	<li><a class="tabs-tab shipping" href="#shipping">Entrega</a></li>
+	<li><a class="tabs-tab payment" href="#payment">Dados Bancários</a></li>
+	<li><a class="tabs-tab social" href="#social">Redes</a></li>
+	<li><a class="tabs-tab" href="..">Gerenciar Loja</a></li>
 </ul>
